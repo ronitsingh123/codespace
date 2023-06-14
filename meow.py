0bdc77551330336fd50ba0ff9682d3fc46ebfa78
ronitsingh123 <133147043+ronitsingh123@users.noreply.github.com>
2023-06-14 12:24:21 +0000

    Wed, Jun 14, 2023, 12:24 PM +00:00

--- meow.py
+++ meow.py
@@ -2,10 +2,11 @@ import sys
 
 names = ["Ron", "Harry", "Hermoine"]
 
-for n in names:
-    name = input("Name: ")
-    if n == name:
+name = input("Name: ")
+
+if name in names:
         print("Found")
         sys.exit(0)
+        
 print("Not found")
 sys.exit(1)
